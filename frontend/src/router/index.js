0b028f326084861f4ed34d24cdf02813c8f2c62c
Toma Gagne <toma.gagne.1@ulaval.ca>
2020-03-22 22:34:18 -0400

    frontend and backend are linked

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HomePage from '@/components/HomePage'
+import HomePage from '../components/HomePage'
+import NotFound from '../components/NotFound'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HomePage',
       component: HomePage
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
     }
   ]
 })
