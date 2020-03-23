d9889b674f945ca99642144a3fe298b9fd188380
Toma Gagne <toma.gagne.1@ulaval.ca>
2020-03-23 18:47:08 -0400

    essayez ça

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,10 @@
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
   render: h => h(App)
-})
+}).$mount("#app");
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,21 +1,27 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HomePage from '../components/HomePage'
-import NotFound from '../components/NotFound'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomePage from "../components/HomePage";
+import NotFound from "../components/NotFound";
 
-Vue.use(Router)
+Vue.use(VueRouter);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HomePage',
-      component: HomePage
-    },
-    {
-      path: '*',
-      name: 'NotFound',
-      component: NotFound
-    }
-  ]
-})
+const routes = [
+  {
+    path: "/",
+    name: "HomePage",
+    component: HomePage
+  },
+  {
+    path: "*",
+    name: "NotFound",
+    component: NotFound
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
