bba60ff7432c46425e84dafb5d38bc072d5c4d48
Toma Gagne <toma.gagne.1@ulaval.ca>
2020-03-30 14:30:32 -0400

    populate brands script done

--- backend/initialisation_script.py
+++ backend/initialisation_script.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
 import pymysql
 import pymysql.cursors
+from .scripts.generate_brands import create_brands
 
 HOST = 'localhost'
 USER = 'root'
@@ -10,14 +10,7 @@ DATABASE = 'glo2005'
 initScriptFilenames = [
     'database/db_init/init_tables.sql',
     'database/db_init/init_types.sql',
-    'database/db_init/init_styles.sql',
-    'database/db_init/init_brands.sql',
-    'database/db_init/init_customers.sql',
-    'database/db_init/init_beers.sql',
-    'database/db_init/init_beerStyles.sql',
-    'database/db_init/init_beerTypes.sql',
-    'database/db_init/init_order_items.sql',
-    'database/db_init/init_orders.sql',
+    'database/db_init/init_styles.sql'
 ]
 
 
@@ -29,6 +22,7 @@ def createDatabaseScript(filename):
 def runAllInitScript():
     for filename in initScriptFilenames:
         executeSqlScriptFromFile(filename)
+    populateBrands()
 
 
 def executeSqlScriptFromFile(filename):
@@ -63,3 +57,20 @@ def executeCommands(sqlCommands):
             conn.commit()
         except Exception as e:
             print(e)
+
+
+def populateBrands():
+    brands = create_brands()
+    try:
+        for brand in brands:
+            conn = pymysql.connect(host=HOST, user=USER, password=PASSWORD, db=DATABASE)
+            cur = conn.cursor()
+            print(brand)
+            add_brand = "INSERT INTO Brands" \
+                        "(brand_id, brand_name, brand_phone, brand_address, brand_city, brand_country)" \
+                        "VALUES (%s, %s, %s, %s, %s, %s)"
+            brand_data = (brand['id'], brand['name'], brand['phone'], brand['address'], brand['city'], brand['country'])
+            cur.execute(add_brand, brand_data)
+            conn.commit()
+    except Exception as e:
+        print(e)
---
+++ backend/scripts/generate_brands.py
@@ -0,0 +1,46 @@
+from faker import Faker
+import json
+from random import randint, choice
+
+fake = Faker()
+
+with open("backend/json_files/countries.json") as jsonfile:
+    countries = json.load(jsonfile)
+
+with open("backend/json_files/cities.json") as jsonfile:
+    cities = json.load(jsonfile)
+
+
+def create_brands():
+    brands = []
+    with open("backend/json_files/beer_brands.json") as jsonfile:
+        data = json.load(jsonfile)
+        for i, brand in enumerate(data):
+            brand_infos = {
+                'id': i,
+                'name': brand['name'],
+                'phone': generatePhoneNumber(),
+                'address': getRandomStreetAddress(),
+                'city': getRandomCity(),
+                'country': getRandomCountry()
+            }
+            brands.append(brand_infos)
+    return brands
+
+
+def generatePhoneNumber():
+    return "1-" + str(randint(100, 999)) + "-" + str(randint(100, 999)) + "-" + str(randint(1000, 9999))
+
+
+def getRandomStreetAddress():
+    address = fake.address()
+    realAddress = address.split("\n")
+    return realAddress[0]
+
+
+def getRandomCountry():
+    return choice(countries)['name']
+
+
+def getRandomCity():
+    return choice(cities)['name']
