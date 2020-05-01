57c00e852736bd966f537db6cc38701ae890779a
Benjamin Girard <benjamin.girard.protic@gmail.com>
2020-05-01 17:11:12 -0400

    mfw tu copy paste

--- backend/blueprints/orders.py
+++ backend/blueprints/orders.py
@@ -2,31 +2,31 @@ from flask import Blueprint, jsonify, request, Response
 
 from service.OrderService import OrderService
 
-beers = Blueprint("orders", __name__, url_prefix="/")
+orders = Blueprint("orders", __name__, url_prefix="/")
 
 
-@beers.route("orders/<order_id>", methods=['GET'])
+@orders.route("orders/<order_id>", methods=['GET'])
 def getOrderByID(order_id):
     orderService = OrderService()
     order = orderService.getOrderByID(order_id).__dict__
     return jsonify(order)
 
 
-@beers.route("orders/customer/<customer_id>", methods=['GET'])
+@orders.route("orders/customer/<customer_id>", methods=['GET'])
 def getOrdersByCustomer(customer_id):
     orderService = OrderService()
     orders = [ order.__dict__ for order in orderService.getOrdersByCustomer(customer_id) ]
     return jsonify(orders)
 
 
-@beers.route("orders/<order_id>/items", methods=['GET'])
+@orders.route("orders/<order_id>/items", methods=['GET'])
 def getOrderItems(order_id):
     orderService = OrderService()
     items = [ item.__dict__ for item in orderService.getOrderItems(order_id) ]
     return jsonify(items)
 
 
-@beers.route("orders/buy", methods=['POST'])
+@orders.route("orders/buy", methods=['POST'])
 def buy():
     items = request.json['items']
     order = request.json['order']
