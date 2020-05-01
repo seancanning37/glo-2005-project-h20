ab52165cc78f7eb549ee8681c5c755696eb2989a
Toma Gagne <46379340+TomaGagne@users.noreply.github.com>
2020-05-01 12:30:22 -0400

    nav bar pour Sean (#8)

--- frontend/src/views/Beer.vue
+++ frontend/src/views/Beer.vue
@@ -1,21 +1,29 @@
 <template>
-  <div>
-    <h1>
-      BEER PAGE
-    </h1>
-    <p>Beer id: {{ this.beer.id }}</p>
-    <p>Beer name: {{ this.beer.name }}</p>
-    <p>Brand id: {{ this.beer.brand_id }}</p>
-    <p>abv: {{ this.beer.abv }}</p>
-    <p>ibu: {{ this.beer.ibu }}</p>
-    <p>volume: {{ this.beer.volume }}</p>
-    <p>price: {{ this.beer.price }}</p>
-    <p>Description: {{ this.beer.description }}</p>
-  </div>
+  <v-container>
+    <v-container>
+      <h1>
+        BEER PAGE
+      </h1>
+      <p>Beer id: {{ this.beer.id }}</p>
+      <p>Beer name: {{ this.beer.name }}</p>
+      <p>Brand id: {{ this.beer.brand_id }}</p>
+      <p>abv: {{ this.beer.abv }}</p>
+      <p>ibu: {{ this.beer.ibu }}</p>
+      <p>volume: {{ this.beer.volume }}</p>
+      <p>price: {{ this.beer.price }}</p>
+      <p>Description: {{ this.beer.description }}</p>
+    </v-container>
+    <v-container>
+      <v-btn v-on:click="addBeerToCart">
+        Add to cart
+      </v-btn>
+    </v-container>
+  </v-container>
 </template>
 
 <script>
 import axios from "axios";
+import Cookie from "js-cookie"
 
 export default {
   name: "Beer",
@@ -46,6 +54,10 @@ export default {
         .catch(error => {
           console.log(error);
         });
+    },
+    addBeerToCart: function() {
+      let cookie = Cookie.get("beerbender-token");
+      console.log(cookie);
     }
   }
 };
