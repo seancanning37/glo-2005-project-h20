fc224e5bb18dfaec0fd673b68af3243235a44573
girardb <benjamin.girard.protic@gmail.com>
2020-05-02 16:35:05 -0400

    lien vers la detailed order (#41)

--- frontend/src/components/order/Order.vue
+++ frontend/src/components/order/Order.vue
@@ -1,7 +1,10 @@
 <template>
     <v-container>
+        <a :href="`/customers/${order.customer_id}/${order.order_id}`">
+            {{ order.order_id }}
+        </a>
         <v-card-text>
-            {{ order.order_id }} {{ order.customer_id }} {{ order.order_date }} {{ order.status }} {{ order.total_price }} {{ order.comment }}
+            {{ order.customer_id }} {{ order.order_date }} {{ order.status }} {{ order.total_price }} {{ order.comment }}
         </v-card-text>
     </v-container>
 </template>
