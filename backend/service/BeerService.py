3fb63e8ec75bb8205b8801c9817636af90459842
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2020-05-02 13:39:34 -0400

    fix la shit a tom

--- backend/service/BeerService.py
+++ backend/service/BeerService.py
@@ -7,12 +7,12 @@ def createBeerFromCursorInfos(beerInfos):
     beer.id = beerInfos[0]
     beer.brand_id = beerInfos[1]
     beer.name = beerInfos[2]
-    beer.abv = str(beerInfos[3])
+    beer.abv = float(beerInfos[3])
     beer.ibu = beerInfos[4]
     beer.volume = beerInfos[5]
     beer.style_id = beerInfos[6]
     beer.type_id = beerInfos[7]
-    beer.price = str(beerInfos[8])
+    beer.price = float(beerInfos[8])
     beer.disponibility = beerInfos[9]
     beer.description = beerInfos[10]
     beer.pictureURL = beerInfos[12]
