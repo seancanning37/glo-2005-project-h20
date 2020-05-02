20b0c9521708bc7b890e202d588538e1f96aab1d
Benjamin Girard <benjamin.girard.protic@gmail.com>
2020-05-02 16:32:52 -0400

    change user settings (frontend)

--- frontend/src/views/CustomerSettings.vue
+++ frontend/src/views/CustomerSettings.vue
@@ -1,11 +1,46 @@
 <template>
   <v-container>
     <v-text-field
-      v-model="name"
+      v-model="parameters.name"
       label="Name"
       prepend-icon="mdi-account"
     ></v-text-field>
-    <v-btn v-on:click="updateName">Update name</v-btn>
+    <v-text-field
+            v-model="parameters.phone"
+            label="Phone"
+            prepend-icon="mdi-account"
+    ></v-text-field>
+    <v-text-field
+            v-model="parameters.email"
+            label="Email"
+            prepend-icon="mdi-account"
+    ></v-text-field>
+    <v-text-field
+            v-model="parameters.username"
+            label="Username"
+            prepend-icon="mdi-account"
+    ></v-text-field>
+    <v-text-field
+            v-model="parameters.address_line_1"
+            label="Address line 1"
+            prepend-icon="mdi-account"
+    ></v-text-field>
+    <v-text-field
+            v-model="parameters.address_line_2"
+            label="Address line 2"
+            prepend-icon="mdi-account"
+    ></v-text-field>
+    <v-text-field
+            v-model="parameters.city"
+            label="City"
+            prepend-icon="mdi-account"
+    ></v-text-field>
+    <v-text-field
+            v-model="parameters.country"
+            label="Country"
+            prepend-icon="mdi-account"
+    ></v-text-field>
+    <v-btn v-on:click="update">Update</v-btn>
   </v-container>
 </template>
 
@@ -16,19 +51,29 @@ export default {
   name: "CustomerSettings",
   data: function() {
     return {
-      name: ""
+      parameters: {
+        name: "",
+        phone: "",
+        email: "",
+        username: "",
+        address_line_1: "",
+        address_line_2: "",
+        city: "",
+        country: ""
+      }
     };
   },
   methods: {
-    updateName: function() {
+    update: function() {
       const path =
         "http://localhost:5000/customers/" +
         this.$route.params.customer_id +
-        "/name";
+        "/modify";
+
+      const modified_parameters = this.getModifiedParameters();
+
       axios
-        .put(path, {
-          newName: this.name
-        })
+        .put(path, modified_parameters)
         .then(response => {
           console.log(response.data);
           this.$router.push({ name: "CustomerPage" });
@@ -36,6 +81,15 @@ export default {
         .catch(error => {
           console.log(error.response.data);
         });
+    },
+    getModifiedParameters: function() {
+      let modified_parameters = {};
+      for (let key in this.parameters) {
+        if (this.parameters[key] !== "") {
+          modified_parameters[key] = this.parameters[key];
+        }
+      }
+      return modified_parameters
     }
   }
 };
