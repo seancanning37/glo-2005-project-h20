f872adb6858ceb2a1983afd3771f8fd195dddcac
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2020-05-05 13:20:56 -0400

    refactor style

--- frontend/src/components/brand/Brand.vue
+++ frontend/src/components/brand/Brand.vue
@@ -1,38 +1,33 @@
 <template>
-    <v-card height="275" width="400">
-        <v-row>
-            <v-col>
-                <v-card-title class="mb-0 pb-0">
-                    <a :href="'/brands/' + brand.id" class="link black--text ">
-                        <p
-                                :class="needsSmallerFont() ? 'title' : 'headline' + ' mb-0 mx-6'"
-                        >
-                            {{ brand.name }}
-                        </p>
-                    </a>
-                    <v-container class="pt-0 my-0">
-                        <p class="text-right subtitle-1">
-                            {{ brand.address }} • {{ brand.city }}
-                            {{ brand.country }} • {{ brand.id }}
-                            {{ brand.name }} • {{ brand.phone }}
-                        </p>
-                    </v-container>
-                </v-card-title>
-            </v-col>
-        </v-row>
-    </v-card>
+  <v-card height="215" width="400">
+    <v-row>
+      <v-col>
+        <v-card-title class="mb-0 pb-0">
+          <a :href="'/brands/' + brand.id" class="link black--text ">
+            <p class="headline">
+              {{ brand.name }}
+            </p>
+          </a>
+          <v-container class="pt-0 my-0">
+            <p class="text-left subtitle-1">
+              {{ brand.address }}
+            </p>
+            <p class="text-left subtitle-1">
+              {{ brand.city }}, {{ brand.country }}
+            </p>
+            <p class="text-left subtitle-1">{{ brand.phone }}</p>
+          </v-container>
+        </v-card-title>
+      </v-col>
+    </v-row>
+  </v-card>
 </template>
 
 <script>
-    export default {
-        name: "Brand",
-        props: ["brand"],
-        methods: {
-            needsSmallerFont() {
-                return this.brand.name.length > 17;
-            }
-        }
-    };
+export default {
+  name: "Brand",
+  props: ["brand"],
+};
 </script>
 
 <style></style>
--- frontend/src/views/BrandsHomePage.vue
+++ frontend/src/views/BrandsHomePage.vue
@@ -1,93 +1,89 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-col>
-                <v-container class="pb-0 my-0">
-                    <v-row class="pb-0 my-0">
-                        <v-col class="pb-0">
-                            <p
-                                    style="position:relative; top: 15%;"
-                                    class="headline text-left mb-0 pl-3"
-                            >
-                                Results {{ this.firstBrandId }}-{{ this.lastBrandId }} of
-                                {{ this.filteredBrands.length }}
-                            </p>
-                        </v-col>
-                    </v-row>
-                </v-container>
-                <v-container v-for="brand in shownBrands" class="pt-0" :key="brand.name">
-                    <Brand :brand="brand"/>
-                </v-container>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-container class="pb-0 my-0">
+          <v-row class="pb-0 my-0">
+            <v-col class="pb-0">
+              <p
+                style="position:relative; top: 15%;"
+                class="headline text-left mb-0 pl-3"
+              >
+                Results {{ this.firstBrandId }}-{{ this.lastBrandId }} of
+                {{ this.filteredBrands.length }}
+              </p>
             </v-col>
-
-            <v-container class="pt-0">
-                <v-row class="pt-0">
-                    <v-spacer />
-                    <v-col class="text-right pr-6 pt-0">
-                        <a
-                                :class="
-                isCurrentPage(n)
-                  ? 'current-page black--text font-weight-bold text-center'
-                  : 'link black--text text-center'
-              "
-                                v-for="n in Math.ceil(this.filteredBrands.length / 12)"
-                                :key="n"
-                                @click="updateBrands(n)"
-                        >
-                            {{ n }}
-                        </a>
-                    </v-col>
-                </v-row>
-            </v-container>
+          </v-row>
+        </v-container>
+        <v-row class="pt-6">
+          <v-col v-for="brand in shownBrands" class="pt-0" :key="brand.name">
+            <Brand :brand="brand" />
+          </v-col>
         </v-row>
-    </v-container>
+      </v-col>
+
+      <v-container class="pt-0" style="position: relative; right: -375px;">
+        <a
+          :class="
+            isCurrentPage(n)
+              ? 'current-page black--text font-weight-bold text-center'
+              : 'link black--text text-center'
+          "
+          v-for="n in Math.ceil(this.filteredBrands.length / 12)"
+          :key="n"
+          @click="updateBrands(n)"
+        >
+          {{ n }}
+        </a>
+      </v-container>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-    import { getAllBrands } from "../api/brand.js";
-    import Brand from "../components/brand/Brand";
+import { getAllBrands } from "../api/brand.js";
+import Brand from "../components/brand/Brand";
 
-    export default {
-        name: "BrandsHomePage",
-        components: {Brand},
-        data: () => ({
-            brands: [],
-            filteredBrands: [],
-            shownBrands: [],
-            firstBrandId: 0,
-            lastBrandId: 12,
-            itemsPerPage: 12,
-            currentPage: 0,
-        }),
-        async created() {
-            this.brands = await this.getAllBrands();
-            this.filteredBrands = this.brands;
-            this.updateBrands(1);
-        },
-        methods: {
-            getAllBrands: async function() {
-                const brands = await getAllBrands();
-                return brands.data;
-            },
-            updateBrands: function(n) {
-                this.currentPage = n;
-                this.firstBrandId = this.itemsPerPage * (n - 1) + 1;
-                this.lastBrandId = this.firstBrandId + this.itemsPerPage - 1;
+export default {
+  name: "BrandsHomePage",
+  components: { Brand },
+  data: () => ({
+    brands: [],
+    filteredBrands: [],
+    shownBrands: [],
+    firstBrandId: 0,
+    lastBrandId: 12,
+    itemsPerPage: 12,
+    currentPage: 0,
+  }),
+  async created() {
+    this.brands = await this.getAllBrands();
+    this.filteredBrands = this.brands;
+    this.updateBrands(1);
+  },
+  methods: {
+    getAllBrands: async function() {
+      const brands = await getAllBrands();
+      return brands.data;
+    },
+    updateBrands: function(n) {
+      this.currentPage = n;
+      this.firstBrandId = this.itemsPerPage * (n - 1) + 1;
+      this.lastBrandId = this.firstBrandId + this.itemsPerPage - 1;
 
-                if (this.lastBrandId > this.filteredBrands.length) {
-                    this.lastBrandId = this.filteredBrands.length;
-                }
-                this.shownBrands = this.filteredBrands.slice(
-                    this.firstBrandId - 1,
-                    this.lastBrandId
-                );
-            },
-            isCurrentPage: function(n) {
-                return n === this.currentPage;
-            },
-        },
-    };
+      if (this.lastBrandId > this.filteredBrands.length) {
+        this.lastBrandId = this.filteredBrands.length;
+      }
+      this.shownBrands = this.filteredBrands.slice(
+        this.firstBrandId - 1,
+        this.lastBrandId
+      );
+    },
+    isCurrentPage: function(n) {
+      return n === this.currentPage;
+    },
+  },
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
