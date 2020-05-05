209e7d552b57087aebdaa8a687a29261b887c408
Benjamin Girard <benjamin.girard.protic@gmail.com>
2020-05-05 12:39:47 -0400

    call getallbrands frontend

---
+++ frontend/src/api/brand.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000";
+
+export const getAllBrands = async () => {
+    const path = `${API_URL}/brands`;
+    try {
+        return axios.get(path);
+    } catch (error) {
+        console.log(error);
+    }
+};
