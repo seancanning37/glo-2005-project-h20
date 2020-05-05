9c13bcac0107a06e704dc7103fd743592da2e50a
Benjamin Girard <benjamin.girard.protic@gmail.com>
2020-05-05 10:56:57 -0400

    order history button sur le profile au lieu de la navbar

--- frontend/src/api/cart.js
+++ frontend/src/api/cart.js
@@ -47,10 +47,6 @@ export const getRewards = () => {
   }
 };
 
-export const getCustomerId = () => {
-  return JSON.parse(Cookies.get("beerbender-token"))["customer_id"];
-}
-
 export const getCartItems = () => {
   const cookie = JSON.parse(Cookies.get("beerbender-token"));
   const cart = cookie["cart"];
