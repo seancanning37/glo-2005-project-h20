29f5e013a2ef8ad8ef7f39d4633e76fdd71ea1b0
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2020-05-05 13:05:25 -0400

    added progress bar for rewards

--- frontend/src/components/cart/CartHeader.vue
+++ frontend/src/components/cart/CartHeader.vue
@@ -3,31 +3,41 @@
     <p class="display-2">
       Cart
     </p>
-    <p>
-      Money earned: {{ money_earned }}
-    </p>
+    <v-container style="width: 500px;">
+      <p class="headline pb-0 mb-0">Reward status</p>
+      <p class="pt-0 mt-0">
+        Spend 100 $ to get free shipping on your next order !
+      </p>
+      <v-progress-linear
+        :value="money_earned"
+        color="yellow darken-3"
+        background-color="blue lighten-3"
+        height="35px"
+        width="150px"
+        striped
+        >{{ money_earned }} / 100</v-progress-linear
+      >
+    </v-container>
   </v-container>
 </template>
 
 <script>
-  import {getRewards} from "../../api/cart";
+import { getRewards } from "../../api/cart";
 
-  export default {
+export default {
   name: "CartHeader",
-    data: function() {
-    return {
-      money_earned: 0
-    }
-    },
-    created: async function () {
-      await this.getRewards()
-    },
-    methods: {
-    getRewards: async function () {
+  data: () => ({
+    money_earned: 0,
+  }),
+  created: async function() {
+    await this.getRewards();
+  },
+  methods: {
+    getRewards: async function() {
       const response = await getRewards();
-      this.money_earned = response.data['money_earned'];
-    }
-    }
+      this.money_earned = response.data["money_earned"] * 100;
+    },
+  },
 };
 </script>
 
